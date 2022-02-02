80893c2d6fb19caffb6384d493f38e8aa67cbfc4
Pjumo <parkjunmozz55@gmail.com>
2022-02-03 04:53:54 +0900

    opencv 연동

--- app/src/main/java/com/example/fingerprint/MainActivity.java
+++ app/src/main/java/com/example/fingerprint/MainActivity.java
@@ -1,14 +1,97 @@
 package com.example.fingerprint;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final static String TAG = MainActivity.class.getClass().getSimpleName();
+    private boolean isOpenCvLoaded = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ImageView imageView = findViewById(R.id.imageView);
+        final ImageView imageView2 = findViewById(R.id.imageView2);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                if(!isOpenCvLoaded)
+                    return;
+                try{
+                    InputStream is = getAssets().open("fp.jpg");
+                    Bitmap bitmap = BitmapFactory.decodeStream(is);
+                    Bitmap bitmap_result = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+                    imageView.setImageBitmap(bitmap);
+
+                    Mat fingerprint = new Mat();
+                    Mat result = new Mat();
+                    Utils.bitmapToMat(bitmap, fingerprint);
+
+                    Imgproc.Canny(fingerprint, result, 50, 150);
+
+                    Utils.matToBitmap(result, bitmap_result);
+                    imageView2.setImageBitmap(bitmap_result);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status){
+            switch(status){
+                case LoaderCallbackInterface.SUCCESS:
+                    Log.i(TAG, "OpenCV loaded successfully");
+                    break;
+                default:
+                    super.onManagerConnected(status);
+                    break;
+            }
+        }
+    };
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        System.loadLibrary("opencv_java4");
+        System.loadLibrary("fingerprint");
+        if(!OpenCVLoader.initDebug()) {
+            Log.d(TAG, "Internal OpenCV library not found. Using openCV Manager for initialization");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION, this, mLoaderCallback);
+        }
+        else{
+            Log.d(TAG, "OpenCV library found inside package. Using it!");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+            isOpenCvLoaded=true;
+        }
     }
 }
